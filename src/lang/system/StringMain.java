56dac3311ef98d15363f3d5e7f288fe65338f7c2
SooYoung Joo <asoostale@gmail.com>
2024-05-21 13:03:30 +0900

    system 마무리

--- src/lang/system/StringMain.java
+++ src/lang/system/StringMain.java
@@ -6,7 +6,7 @@ public class StringMain {
     public static void main(String[] args) {
         char[] arrayOrigin = {'h', 'e', 'l', 'l', 'o'};
         char[] copiedArray = new char[5];
-        System.arraycopy(arrayOrigin, 0,copiedArray, 0, arrayOrigin.length);
+        System.arraycopy(arrayOrigin, 0, copiedArray, 0, arrayOrigin.length);
 
         System.out.println(copiedArray);
 
@@ -17,6 +17,30 @@ public class StringMain {
 
         String result = Arrays.toString(copiedArray);
         System.out.println("result = " + result);
+        System.out.println("=====================================================");
+        char[] test1 = {'a', 's', 'o', 'o', 's', 't', 'a', 'l', 'e'};
+        char[] test2 = new char[9];
+        long nowTime = System.currentTimeMillis();
+        for (int i = 0; i < test1.length; i++) {
+            test2[i] = test1[i];
+        }
+        String result2 = Arrays.toString(test2);
+        long lateTime = System.currentTimeMillis();
+        long resultTime1 = lateTime - nowTime;
+
+        char[] test3 = {'a', 's', 'o', 'o', 's', 't', 'a', 'l', 'e'};
+        char[] test4 = new char[9];
+        long nowTime2 = System.currentTimeMillis();
+        System.arraycopy(test3, 0, test4, 0, test3.length);
+        String result5 = Arrays.toString(test4);
+        long lateTime2 = System.currentTimeMillis();
+        long result3 = lateTime2 - nowTime2;
+
+        if (resultTime1 > result3) {
+            System.out.println("System.arrayCopy가 더 빠릅니다");
+        }
+        System.out.println("테스트가 의미 없습니다");
+
 
     }
 }
