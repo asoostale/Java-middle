0eef06a9949fde6aee26b3729270e1a52300a3db
SooYoung Joo <asoostale@gmail.com>
2024-05-21 12:50:33 +0900

    System.arrayCopy();

--- src/lang/system/StringMain.java
+++ src/lang/system/StringMain.java
@@ -1,13 +1,22 @@
 package lang.system;
 
+import java.util.Arrays;
+
 public class StringMain {
     public static void main(String[] args) {
         char[] arrayOrigin = {'h', 'e', 'l', 'l', 'o'};
         char[] copiedArray = new char[5];
         System.arraycopy(arrayOrigin, 0,copiedArray, 0, arrayOrigin.length);
 
-        for (char c : copiedArray) {
-            System.out.println("c = " + c);
-        }
-           }
+        System.out.println(copiedArray);
+
+        String test = "hello";
+        System.out.println(test.equals(copiedArray));
+
+        System.out.println(copiedArray[0]);
+
+        String result = Arrays.toString(copiedArray);
+        System.out.println("result = " + result);
+
+    }
 }
